ed06a60c0d0fd46b0013a4b57f64d5315ce8b528
Thibault Van de Sompele <thibaultenmeer@gmail.com>
2021-02-06 21:09:43 +0100

    Saving skills

--- src/main/java/com/example/application/services/SkillParser.java
+++ src/main/java/com/example/application/services/SkillParser.java
@@ -6,79 +6,69 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class SkillParser {
-    private JSONParser jParser;
-    private JSONArray skillsArray=null;
+    private JSONObject skillsArray = null;
+    private final String path = "skills.json";
 
     public SkillParser(){
-        jParser = new JSONParser();
-
+        loadSkills();
     }
 
-    public void loadFile(String path){
-        try (FileReader fr = new FileReader(path)) {
-            Object obj = jParser.parse(fr);
+    public void loadSkills(){
+        JSONParser jParser = new JSONParser();
 
-            skillsArray = (JSONArray) obj;
+        try (FileReader file = new FileReader(path)) {
+            skillsArray = (JSONObject) jParser.parse(file);
 
-
-        }catch(FileNotFoundException f){
+        } catch(IOException | ParseException f) {
             f.printStackTrace();
-        }catch(IOException io){
-            io.printStackTrace();
-        } catch (ParseException e) {
+        }
+    }
+
+    public void newSkill(String question) {
+        if (skillsArray.get(question) == null) {
+            skillsArray.put(question, getSkillTemplate());
+
+            updateFile();
+        }
+    }
+
+    public void addSlot(String question, String slot) {
+        JSONObject parameters = (JSONObject) skillsArray.get(question);
+        JSONArray slots = (JSONArray) parameters.get("slots");
+
+        if (!slots.contains(slot)) {
+            slots.add(slot);
+            updateFile();
+        }
+    }
+
+    public void addAction(String question, String action, JSONObject conditions) {
+        JSONObject parameters = (JSONObject) skillsArray.get(question);
+        JSONObject actions = (JSONObject) parameters.get("actions");
+
+        actions.put(action, conditions);
+        updateFile();
+    }
+
+    private JSONObject getSkillTemplate() {
+        JSONObject skillTemplate = new JSONObject();
+        skillTemplate.put("slots", new JSONArray());
+        skillTemplate.put("actions", new JSONObject());
+
+        return skillTemplate;
+    }
+
+    private void updateFile() {
+        try (FileWriter file = new FileWriter(path)){
+            file.write(skillsArray.toJSONString());
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-    public String query(String query){
-        loadFile("skills.JSON");
-        for(Object o: skillsArray){
-            JSONArray jsArray = (JSONArray) o;
-            if(jsArray.toString().contains(query)){ //TODO: verify if it's the right way to do it
-
-                String result = "";
-
-                Skill skill = makeSkill(jsArray);
-
-                return skill.getAction(); // ??
-            }
-        }
-        return "";
-    }
-
-    private Skill makeSkill(JSONArray jsArray) {
-        //TODO: make the skill object
-        return null;
-    }
-
-    public void makeSkill(String query){
-        if(skillsArray==null) loadFile("skills.JSON");
-
-        JSONObject newSkill = new JSONObject();
-        JSONObject contentOfSkill = new JSONObject();
-
-        String firstLine = query.substring(0, query.indexOf("\n")); //TODO: check if newline is \n or \r or something else
-
-        //TODO: insert each line in a JSON object
-
-
-        newSkill.put(firstLine, contentOfSkill);
-
-        skillsArray.add(newSkill);
-
-
-    }
-
-
-    public String[] splitSkill(String skill) {
-
-        return skill.split(" ");
-    }
-
 }
