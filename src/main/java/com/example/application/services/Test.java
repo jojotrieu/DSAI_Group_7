37e44be5b837c595ae45d3c5e25c4f339f05096e
Thibault Van de Sompele <thibaultenmeer@gmail.com>
2021-02-09 11:49:09 +0100

    Delete Test.java

--- src/main/java/com/example/application/services/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        SkillParser parser = new SkillParser();
-        parser.splitSkill("Question where is <person> at <time>");
-        parser.makeSkill("Question where is <person> at <time>");
-
-    }
-}
