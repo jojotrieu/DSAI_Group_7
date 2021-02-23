19bd75ec9985ddab0baa6f170e07457d5bd65f16
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2021-02-23 22:33:40 +0200

    update ChatBotView

--- src/main/java/com/example/application/views/chatbotview/ChatBotView.java
+++ src/main/java/com/example/application/views/chatbotview/ChatBotView.java
@@ -10,12 +10,12 @@ import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.views.main.MainView;
 import com.vaadin.flow.router.RouteAlias;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,29 +28,31 @@ public class ChatBotView extends HorizontalLayout {
     private TextField questionTextField;
     private Button clearButton = new Button("Clear Chat");
     private H4 thinking = new H4("ChatBot: Mmmm... Let me think.");
+    private TextArea area = new TextArea();
     private ChatBot chatBot = new ChatBot();
+    private String conversation = "";
 
     public ChatBotView() {
         setId("chatbot-view");
-        List<H4> exchange = new ArrayList<>();
+        area.setReadOnly(true);
+        area.setId("text-area");
         questionTextField = new TextField("Ask me anything");
-        questionTextField.setSizeFull();
+        questionTextField.setId("question-field");
         clearButton.setId("clear-button");
         questionTextField.addKeyPressListener(Key.ENTER, e -> {
             //disable Text Field while ChatBot is thinking
             questionTextField.setEnabled(false);
             //display question in H4
-            H4 questionH4 = new H4("You: " + questionTextField.getValue());
-            add(questionH4);
-            exchange.add(questionH4);
+            String questionH4 = "You: " + questionTextField.getValue();
+            conversation += questionH4 + "\n";
             //display "thinking" while ChatBot is thinking
             add(thinking);
             //get response from chatBot
             try {
                 String responseString = chatBot.response(questionTextField.getValue());
-                H4 responseH4 = new H4("ChatBot: " + responseString);
-                add(responseH4);
-                exchange.add(responseH4);
+                String responseH4 = "ChatBot: " + responseString;
+                conversation += responseH4 + "\n";
+                area.setValue(conversation);
             } catch (InterruptedException interruptedException) {
                 interruptedException.printStackTrace();
             }
@@ -63,10 +65,10 @@ public class ChatBotView extends HorizontalLayout {
         });
         add(questionTextField);
         add(clearButton);
+        add(area);
         clearButton.addClickListener(e -> {
-            for (H4 h4 : exchange) {
-                remove(h4);
-            }
+            conversation = "";
+            area.setValue(conversation);
             clearButton.setEnabled(false);
         });
         clearButton.setEnabled(false);
