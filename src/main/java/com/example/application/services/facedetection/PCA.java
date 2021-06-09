9439d4f90b8b29032be9135403c948718f19652a
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2021-06-09 15:56:30 +0200

    create structure to import the data

--- src/main/java/com/example/application/services/facedetection/PCA.java
+++ src/main/java/com/example/application/services/facedetection/PCA.java
@@ -1,4 +1,49 @@
 package com.example.application.services.facedetection;
 
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PCA {
+    private List<Mat> trainingImages = new ArrayList<>();
+    private List<Integer> labels = new ArrayList<>();
+
+
+    //TODO: Train using data.
+    public void trainRecognizer() {
+
+    }
+
+    //TODO: After it's trained we can project onto the eigenspace and find the most similar image in our database.
+    public String recognizeFace(Mat currentImage) {
+        return "0";
+    }
+
+    public void readData(String path) throws FileNotFoundException {
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(path));
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] data = line.split(";");
+                // Read Images in Gray format
+                Mat readImage = Imgcodecs.imread(data[0], 0);
+                this.trainingImages.add(readImage);
+                // Collect actual labels
+                this.labels.add(Integer.parseInt(data[1]));
+                line = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ src/main/java/com/example/application/services/facedetection/TestPCA.java
@@ -0,0 +1,7 @@
+package com.example.application.services.facedetection;
+
+public class TestPCA {
+    public static void main(String[] args){
+        // Class to test the PCA when it's implemented
+    }
+}
