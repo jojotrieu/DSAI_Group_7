9fdbe08570570b10f13daeb381ecfd3dbe4d8e5c
RoscaAlex00 <56193131+RoscaAlex00@users.noreply.github.com>
2021-06-11 21:07:41 +0300

    PCA works?

--- src/main/java/com/example/application/services/facedetection/PCA.java
+++ src/main/java/com/example/application/services/facedetection/PCA.java
@@ -1,46 +1,58 @@
 package com.example.application.services.facedetection;
 
+import aist.science.aistcv.AistCVLoader;
+import com.lowagie.text.html.simpleparser.Img;
+import nu.pattern.OpenCV;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.face.BasicFaceRecognizer;
 import org.opencv.face.EigenFaceRecognizer;
-import org.opencv.face.FaceRecognizer;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.opencv.core.CvType.CV_32SC1;
+import static org.opencv.core.CvType.channels;
 
 public class PCA {
     private List<Mat> trainingImages = new ArrayList<>();
     private List<Integer> labelList = new ArrayList<>();
-    private BasicFaceRecognizer faceRecognizer = EigenFaceRecognizer.create(80);
-
+    private BasicFaceRecognizer faceRecognizer;
 
     public void trainRecognizer() {
+        AistCVLoader.loadLocally();
+        faceRecognizer = EigenFaceRecognizer.create(1000,13000);
         Mat labels = new Mat(labelList.size(), 1, CV_32SC1);
         for (int i = 0; i < labelList.size(); i++) {
-            labels.put(i, 1, labelList.get(i));
+            labels.put(i, 0, labelList.get(i));
         }
         faceRecognizer.train(trainingImages, labels);
     }
 
     public String recognizeFace(Mat currentImage) {
+        Imgproc.cvtColor(currentImage, currentImage, Imgproc.COLOR_BGR2GRAY);
         int label = faceRecognizer.predict_label(currentImage);
+        System.out.println(label);
         if (label == 1) {
             return "Alex";
         } else if (label == 2) {
             return "Corina";
-        } else {
+        } else if (label == 3) {
+            return "Adele";
+        }
+        else{
             return "I can't recognize you!";
         }
     }
 
-    public void readData(String path) throws FileNotFoundException {
+    public void readData(String path) {
         BufferedReader br;
         try {
             br = new BufferedReader(new FileReader(path));
@@ -49,9 +61,13 @@ public class PCA {
             while (line != null) {
                 String[] data = line.split(";");
                 // Read Images in Gray format
-                Mat readImage = Imgcodecs.imread(data[0], 0);
-
+                Mat readImage = Imgcodecs.imread(data[0]);
+                Imgproc.cvtColor(readImage, readImage, Imgproc.COLOR_BGR2GRAY);
+                if (readImage.rows() != 240 || readImage.cols() != 320) {
+                    Imgproc.resize(readImage, readImage, new Size(320, 240));
+                }
                 this.trainingImages.add(readImage);
+                Imgcodecs.imwrite("adele.jpg",readImage);
                 // Collect actual labels
                 this.labelList.add(Integer.parseInt(data[1]));
                 line = br.readLine();
--- src/main/java/com/example/application/services/facedetection/TestPCA.java
+++ src/main/java/com/example/application/services/facedetection/TestPCA.java
@@ -1,13 +1,14 @@
 package com.example.application.services.facedetection;
 
 import nu.pattern.OpenCV;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
 import java.awt.image.BufferedImage;
 import java.io.FileNotFoundException;
 
 public class TestPCA {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Camera camera = new Camera();
         camera.turnOnCamera();
         BufferedImage img = camera.captureImage();
@@ -17,7 +18,7 @@ public class TestPCA {
         Mat converted = det.BufferedImageToMat(img);
 
         PCA pca = new PCA();
-        pca.readData("\\RecognizerDB\\TrainDataPath.txt");
+        pca.readData("C:\\Users\\dolbi\\Desktop\\DSAI_Group_7\\RecognizerDB\\TrainDataPath.txt");
         pca.trainRecognizer();
         String output = pca.recognizeFace(converted);
         System.out.println("Hello, " + output + " !");
