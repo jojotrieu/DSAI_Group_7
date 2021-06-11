98387ae1e106139f27f9c71c27cef8d7902ae058
Adeleimpa <56159423+Adeleimpa@users.noreply.github.com>
2021-06-11 23:31:01 +0200

    nvm

--- src/main/java/com/example/application/services/chatbot/Rule.java
+++ src/main/java/com/example/application/services/chatbot/Rule.java
@@ -22,30 +22,6 @@ public class Rule {
         return copy;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getVariable() {
-        return variable;
-    }
-
-    public void setVariable(String var) {
-        this.variable = var;
-    }
-
-    public List<String> getExpressions() {
-        return expressions;
-    }
-
-    public void setExpressions(List<String> expr) {
-        this.expressions = expr;
-    }
-
     public static String expressionToString(List<String> expressions) {
         String expr = expressions.toString();
         expr = expr.substring(1, expr.length() - 1);
